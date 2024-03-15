f5c4bca38cf2fc1e5686906c9341085c4a683ff5
India <tanmoy587@gmail.com>
2024-03-15 08:32:19 +0530

    280. Introduction to JSX

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+
+// var h1=document.createElement("h1");
+// h1.innerHTML="Hello";
+// document.getElementById("root").append(h1);
+                 
+
+render(
+  <div>
+    <h1>Hello</h1>
+    <p>This is Paragraph</p>
+  </div>,
+  document.getElementById("root")
+  );
